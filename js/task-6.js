5e115074ab18c9d9e45e65209b154cdc0c3dc0ca
Fedor-N <nfedorf@gmail.com>
2024-05-20 17:08:45 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -13,42 +13,43 @@
 //*Усі елементи повинні мати випадковий колір фону. Використовуй готову функцію getRandomHexColor() для отримання випадкового кольору.
 //*Для очищення колекції після натискання на кнопку Destroy створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
 
-const input = document.querySelector("input");
-const createBtn = document.querySelector("[data-create]");
-const destroyBtn = document.querySelector("[data-destroy]");
-const boxesContainer = document.getElementById("boxes");
 
-createBtn.addEventListener("click", createBoxes);
-destroyBtn.addEventListener("click", destroyBoxes);
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
 
-function createBoxes() {
-  const amount = input.value;
-
-  if (amount < 1 || amount > 100) {
-    alert("Please enter a number between 1 and 100.");
-    return;
+createBtn.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount > 1 || amount < 100) {
+    destroyBoxes();
+    const boxesArray = [];
+    for (let i = 0; i < amount; i += 1) {
+      boxesArray.push(createBoxes(i));
+    }
+    input.value = '';
+    boxesContainer.append(...boxesArray);
   }
+});
 
+destroyBtn.addEventListener('click', () => {
   destroyBoxes();
-
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-    const size = 30 + i * 10;
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
-  }
-
-  input.value = "";
-}
-
-function destroyBoxes() {
-  boxesContainer.innerHTML = "";
-}
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, "0")}`;
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const box = document.createElement('div');
+  box.style.width = `${30 + amount * 10}px`;
+  box.style.height = `${30 + amount * 10}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  return box;
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
 }
